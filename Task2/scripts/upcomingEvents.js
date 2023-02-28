fb7bb1db37f780a8b8e40c1031f6bc8c998913aa
Matias Calandri <matiascalandri0388@gmail.com>
2023-02-28 10:23:37 -0300

    version 1

---
+++ Task2/scripts/upcomingEvents.js
@@ -0,0 +1,19 @@
+const contenedorTarjetas = document.querySelector('#contenedor-cards')
+
+let tarjetas = ''
+
+for (const evento of data.events) {
+    if (evento.date >= data.currentDate ){
+      tarjetas += `<div class="card" style="width: 18rem; height=20rem;">
+      <img src="${evento.image}" class="card-img-top" alt="evento" height="180">
+      <div class="card-body">
+        <h5 class="card-title">${evento.name}</h5>
+        <p class="card-text p-3">${evento.description}</p>
+        <a href="./details.html" class="btn btn-primary position-absolute bottom-0 end-0 ms-auto m-2">Details</a>
+      </div>
+    </div>`
+
+}
+}
+
+contenedorTarjetas.innerHTML = tarjetas
